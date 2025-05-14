6b5c8a17e1a1abb6f61eb5059994f01530b1204f
hunghx <baby10051999@gmail.com>
2025-05-14 21:15:14 +0700

    handle conflict revert

--- src/main/java/ra/security/controller/MailController.java
+++ src/main/java/ra/security/controller/MailController.java
@@ -6,14 +6,19 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.multipart.MultipartFile;
 import ra.security.model.dto.request.MailRequestDto;
 import ra.security.service.EmailService;
+import ra.security.service.UploadService;
 
 import java.io.IOException;
 
 @RestController
 public class MailController {
+    @Autowired
+    private UploadService uploadService;
     @Autowired
     private EmailService emailService;
 
@@ -21,6 +26,12 @@ public class MailController {
     public ResponseEntity<?> sendEmailWithAttachment(@ModelAttribute MailRequestDto requestDto) throws MessagingException, IOException {
         emailService.sendEmailWithAttachment(requestDto.getRecipientEmail(),requestDto.getCcEmails(),requestDto.getBccEmails(), requestDto.getSubject(), requestDto.getHtmlContent(), requestDto.getAttachments());
         return new ResponseEntity<>("Gư mail thành công", HttpStatus.OK);
+    }
+
+    @PostMapping("/upload")
+    public ResponseEntity<String> upload(@RequestParam MultipartFile file) throws IOException {
+        String url = uploadService.uploadFile(file);
+        return new ResponseEntity<>(url, HttpStatus.OK);
     }
 
 }
--- src/main/java/ra/security/service/EmailService.java
+++ src/main/java/ra/security/service/EmailService.java
@@ -44,7 +44,6 @@ public class EmailService {
                 helper.addAttachment(attachment.getOriginalFilename(), new ByteArrayResource(attachment.getBytes()));
             }
         }
-
         mailSender.send(message);
     }
 
